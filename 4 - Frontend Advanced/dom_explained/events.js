e546791656471c6812fc6ccdc03991da234348d1
Bisharah Estephan <macbosh@Bisharahs-MacBook-Pro.local>
2022-10-30 09:14:11 +0200

    js

---
+++ 4 - Frontend Advanced/dom_explained/events.js
@@ -0,0 +1,7 @@
+function changeBoxColor() {
+  let box = document.getElementById("box").style;
+  box.backgroundColor = "red";
+  box.border = "5px solid blue";
+}
+
+function printDataToTheConsole() {}
