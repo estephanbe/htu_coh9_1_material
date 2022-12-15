872638fc904e7eb6d6fe60dc0ffe3fb288342a92
Bisharah Estephan <macbosh@Bisharahs-MacBook-Pro.local>
2022-12-15 17:48:51 +0300

    CMS

--- cms/resources/views/partials/header-admin.php
+++ cms/resources/views/partials/header-admin.php
@@ -11,7 +11,7 @@ use Core\Helpers\Helper; ?>
     <title>News Agency</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?= "http://" . $_SERVER['HTTP_HOST'] ?>/resources/css/styles.css">
+    <link rel="stylesheet" href="<?= $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['HTTP_HOST'] ?>/resources/css/styles.css">
 </head>
 
 <body class="admin-view">
